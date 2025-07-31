011ee17061530cc08c4526538076e893144de368
Emanuel Correia <guardeded@gmail.com>
2025-07-31 16:47:57 +0100

    Set up the seeders and factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Company;
+use App\Models\Employee;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,5 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(AdminUserSeeder::class);
+
+        Company::factory(10)->create()->each(function ($company) {
+            // Create 5 employees for each company
+            Employee::factory(rand(1, 10))->create([
+                'company_id' => $company->id,
+            ]);
+        });
     }
 }
